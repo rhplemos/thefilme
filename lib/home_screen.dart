1093458c465851475f415da4764e2254eaf3d886
Rafael Lemos <rhplemos@gmail.com>
2024-03-19 19:55:49 -0300

    feat: home screen first form

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,10 +1,70 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text(
+          'The Filme',
+          style: GoogleFonts.aBeeZee(fontSize: 35),
+        ),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Trending Movies',
+                style: GoogleFonts.aBeeZee(fontSize: 25),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: CarouselSlider.builder(
+                  itemCount: 10,
+                  options: CarouselOptions(
+                      height: 300,
+                      autoPlay: true,
+                      viewportFraction: 0.45,
+                      enlargeCenterPage: true,
+                      pageSnapping: true,
+                      autoPlayCurve: Curves.fastOutSlowIn,
+                      autoPlayAnimationDuration: const Duration(seconds: 1)),
+                  itemBuilder: (context, itemIndex, pageViewIndex) {
+                    return ClipRRect(
+                      borderRadius: BorderRadius.circular(12),
+                      child: Container(
+                        height: 300,
+                        width: 200,
+                        color: Colors.amber,
+                      ),
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
