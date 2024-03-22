868a97ec56be926530fa5409fda856fbe3016b96
Rafael Lemos <rhplemos@gmail.com>
2024-03-22 20:18:50 -0300

    feat: detail screen

---
+++ lib/details/DetailsScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:thefilme/models/movie.dart';
+
+class DetailsScreen extends StatelessWidget {
+  const DetailsScreen({
+    super.key,
+    required this.movie,
+  });
+
+  final Movie movie;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
