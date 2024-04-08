36f56bc042cd85c48fb925672e1c755ac580bb8e
Rafael Lemos <rhplemos@gmail.com>
2024-04-08 18:42:53 -0300

    feat: trending row getx

--- lib/widgets/trending_row.dart
+++ lib/widgets/trending_row.dart
@@ -1,71 +1,75 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:thefilme/constants.dart';
 import 'package:thefilme/details/details_screen.dart';
+import 'package:thefilme/home/home_controller.dart';
+import 'package:thefilme/models/movie.dart';
 
-class TrendingRow extends StatelessWidget {
-  const TrendingRow({
-    Key? key,
-    required this.title,
-    required this.snapshot,
-  }) : super(key: key);
-
+class TrendingRow extends GetView<HomeController> {
   final String title;
-  final AsyncSnapshot snapshot;
+  final RxList<Movie> movies;
+
+  TrendingRow(this.movies, {super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          title,
-          style: GoogleFonts.aBeeZee(fontSize: 25),
-        ),
-        const SizedBox(
-          height: 16,
-        ),
+        Obx(() {
+          if (movies.isEmpty) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            return Text(
+              title,
+              style: GoogleFonts.aBeeZee(fontSize: 25),
+            );
+          }
+        }),
+        const SizedBox(height: 16),
         SizedBox(
           width: double.infinity,
-          child: CarouselSlider.builder(
-            itemCount: snapshot.data!.length,
-            options: CarouselOptions(
-              height: 300,
-              autoPlay: true,
-              viewportFraction: 0.45,
-              enlargeCenterPage: true,
-              pageSnapping: true,
-              autoPlayCurve: Curves.fastOutSlowIn,
-              autoPlayAnimationDuration: const Duration(seconds: 1),
-            ),
-            itemBuilder: (context, itemIndex, pageViewIndex) {
-              return GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => DetailsScreen(
-                        movie: snapshot.data[itemIndex],
+          child: Obx(() {
+            if (movies.isEmpty) {
+              return const Center(child: CircularProgressIndicator());
+            } else {
+              return CarouselSlider.builder(
+                itemCount: movies.length,
+                options: CarouselOptions(
+                  height: 300,
+                  autoPlay: true,
+                  viewportFraction: 0.45,
+                  enlargeCenterPage: true,
+                  pageSnapping: true,
+                  autoPlayCurve: Curves.fastOutSlowIn,
+                  autoPlayAnimationDuration: const Duration(seconds: 1),
+                ),
+                itemBuilder: (context, itemIndex, pageViewIndex) {
+                  return GestureDetector(
+                    onTap: () {
+                      Get.to(() => DetailsScreen(
+                        movie: movies[itemIndex],
+                      ));
+                    },
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(12),
+                      child: SizedBox(
+                        height: 300,
+                        width: 200,
+                        child: Image.network(
+                          '${Constants.imagePath}${movies[itemIndex].posterPath}',
+                          filterQuality: FilterQuality.high,
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
                   );
                 },
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(12),
-                  child: SizedBox(
-                    height: 300,
-                    width: 200,
-                    child: Image.network(
-                      filterQuality: FilterQuality.high,
-                      fit: BoxFit.cover,
-                      '${Constants.imagePath}${snapshot.data[itemIndex].posterPath}',
-                    ),
-                  ),
-                ),
               );
-            },
-          ),
+            }
+          }),
         ),
       ],
     );
